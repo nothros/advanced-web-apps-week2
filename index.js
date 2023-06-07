12b17c0f3e9b48ff8ab6680ac9a39a3de1a3bb06
Erika Horros-Wallin <erika.horros@gmail.com>
2023-06-07 22:20:51 +0300

    Add week2 exercise

---
+++ index.js
@@ -0,0 +1,45 @@
+// Requiring express in our server
+const express = require('express');
+const app = express();
+const path = require("path");
+
+const PORT = 3000;
+ 
+app.use(express.json());
+app.use(express.static(path.join(__dirname,'./static')));
+app.use(express.urlencoded({ extended: true }));
+
+let texts = [];
+
+app.get('/hello',(req, res) => {
+  res.json({
+    msg: "Hello world"
+  });
+});
+
+app.post("/list", async (req, res) => {
+  const textItem = await req.body.text
+  texts.push(textItem)
+  res.json({text:texts});
+  
+
+})
+
+app.get('/echo/:id', (req, res) => {
+  res.json(req.params);
+});
+
+app.post('/sum', (req, res) => {
+  const numbers = req.body.numbers;
+  const sum = numbers.reduce((a, b) => a + b, 0)
+  res.json({sum: sum});
+  
+})
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
